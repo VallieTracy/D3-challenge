cac7e9b5d4eee2e1a8180c95ac7ffd570f46ec2e
Vallie Tracy <55700939+VallieTracy@users.noreply.github.com>
2020-03-30 13:12:47 -0500

    have chart on the web, sizing needs to be retooled

---
+++ D3_data_journalism/assets/js/app.js
@@ -0,0 +1,83 @@
+function makeResponsive() {
+
+    // Empty SVG area
+    var svgArea = d3.select("body").select("svg");
+    if (!svgArea.empty()) {
+        svgArea.remove();
+    }
+
+    var svgWidth = window.innerWidth;
+    var svgHeight = window.innerHeight;
+  
+    var margin = {
+      top: 50,
+      bottom: 50,
+      right: 50,
+      left: 50
+    };
+  
+    var height = svgHeight - margin.top - margin.bottom;
+    var width = svgWidth - margin.left - margin.right;
+
+    // Append SVG element to #scatter
+    var svg = d3
+        .select("#scatter")
+        .append("svg")
+        .attr("height", svgHeight)
+        .attr("width", svgWidth);
+    
+    // Append group element
+    var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    d3.csv("assets/data/data.csv").then(function(popData) {
+        // Convert strings into numbers
+        popData.forEach(function(data) {
+            data.poverty = +data.poverty;
+            data.healthcare = +data.healthcare;
+        });
+
+        // Create scales
+        var xLinearScale = d3.scaleLinear()
+            .domain([0, d3.max(popData, d => d.poverty)])
+            .range([0, width]);
+
+        var yLinearScale = d3.scaleLinear()
+            .domain([0, d3.max(popData, d => d.healthcare)])
+            .range([height, 0]);
+
+        // Create axes
+        var bottomAxis = d3.axisBottom(xLinearScale);
+        var leftAxis = d3.axisLeft(yLinearScale);
+
+        // Append axes  
+        chartGroup.append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .call(bottomAxis);
+
+        chartGroup.append("g")
+            .call(leftAxis);
+
+        // Create circles
+        var circlesGroup = chartGroup.selectAll("circle")
+            .data(popData)
+            .enter()
+            .append("circle")
+            .attr("cx", d => xLinearScale(d.poverty))
+            .attr("cy", d => yLinearScale(d.healthcare))
+            .attr("r", "15")
+            .attr("fill", "pink")
+            .attr("opacity", ".5");
+
+
+    }).catch(function(error) {
+        console.log(error);
+      });
+
+} 
+
+// When the browser loads, makeResponsive() is called.
+makeResponsive();
+
+// When the browser window is resized, makeResponsive() is called.
+d3.select(window).on("resize", makeResponsive);

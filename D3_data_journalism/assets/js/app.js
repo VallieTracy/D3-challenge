9f71644b8f37a68b2594e27534699115a191a14a
Vallie Tracy <55700939+VallieTracy@users.noreply.github.com>
2020-03-31 10:06:22 -0500

    needs to be resized, plus get state abbr's inside circles

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -25,7 +25,10 @@ function makeResponsive() {
         .append("svg")
         .attr("height", svgHeight)
         .attr("width", svgWidth);
-    
+
+    var height = svgHeight - margin.top - margin.bottom;
+    var width = svgWidth - margin.left - margin.right;
+
     // Append group element
     var chartGroup = svg.append("g")
         .attr("transform", `translate(${margin.left}, ${margin.top})`);
@@ -39,16 +42,16 @@ function makeResponsive() {
 
         // Create scales
         var xLinearScale = d3.scaleLinear()
-            .domain([0, d3.max(popData, d => d.poverty)])
+            .domain([8, d3.max(popData, d => d.poverty) * 1.05])
             .range([0, width]);
 
         var yLinearScale = d3.scaleLinear()
-            .domain([0, d3.max(popData, d => d.healthcare)])
+            .domain([4, d3.max(popData, d => d.healthcare) * 1.1])
             .range([height, 0]);
 
         // Create axes
-        var bottomAxis = d3.axisBottom(xLinearScale);
-        var leftAxis = d3.axisLeft(yLinearScale);
+        var bottomAxis = d3.axisBottom(xLinearScale).ticks(7);
+        var leftAxis = d3.axisLeft(yLinearScale).ticks(11);
 
         // Append axes  
         chartGroup.append("g")
@@ -66,9 +69,33 @@ function makeResponsive() {
             .attr("cx", d => xLinearScale(d.poverty))
             .attr("cy", d => yLinearScale(d.healthcare))
             .attr("r", "15")
-            .attr("fill", "pink")
-            .attr("opacity", ".5");
+            .attr("class", "stateCircle stateText")
+            .attr("opacity", ".5")
+            .attr("text", function(d) {
+                return(`${d.abbr}`);
+            })
+            .html(function(d) {
+                return(`${d.abbr}`);
+            });
 
+        // Initialize tool tip
+        var toolTip = d3.tip()
+            .attr("class", "d3-tip")
+            .html(function(d) {
+                return(`${d.state}<br>Poverty: ${d.poverty}%<br>Healthcare: ${d.healthcare}%`);
+            });
+
+        // Create tool tip in the chart
+        chartGroup.call(toolTip);
+
+        // Create event listeners to display & hide the tool tip
+        circlesGroup.on("mouseover", function(data) {
+            toolTip.show(data);
+        })
+            .on("mouseout", function(data, index) {
+                toolTip.hide(data);
+            });
+        
         // Create axes labels
         // X
         chartGroup.append("text")
@@ -81,12 +108,8 @@ function makeResponsive() {
             .attr("transform", "rotate(-90)")
             .attr("x", 0 - (height / 2))
             .attr("y", 0 - margin.left + 40)
-            .text("Lacks Healthcare");
-
-        
-          
-        
-    
+            .attr("class", "aText")
+            .text("Lacks Healthcare (%)");
     }).catch(function(error) {
         console.log(error);
       });
